9e1c17d61aa50a63963a47e52e05e487f9194f30
TOBAIS123 <erfanyeganehfar@gmail.com>
2019-06-02 15:22:45 -0400

    m

--- Bill Bai Erfan Yeganehfar SAFEGAMES/core/src/com/safety4kids/game/Utils/InputProcessor.java
+++ Bill Bai Erfan Yeganehfar SAFEGAMES/core/src/com/safety4kids/game/Utils/InputProcessor.java
@@ -20,11 +20,11 @@ public class InputProcessor {
     }
 
     public static void inputProcess() {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.UP))
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP) || Gdx.input.isKeyJustPressed(Input.Keys.W))
             player.jump();
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.b2body.getLinearVelocity().x <= MAX_VELOCITY)
+        if ((Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) && player.b2body.getLinearVelocity().x <= MAX_VELOCITY)
             player.b2body.applyLinearImpulse(new Vector2(0.1f, 0),player.b2body.getWorldCenter(), true);
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.b2body.getLinearVelocity().x >= MIN_VELOCITY)
+        if ((Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) && player.b2body.getLinearVelocity().x >= MIN_VELOCITY)
             player.b2body.applyLinearImpulse(new Vector2(-0.1f, 0),player.b2body.getWorldCenter(), true);
     }
 }
