ee9b9694cc020105524b473d878875bbbf75b5c2
TOBAIS123 <erfanyeganehfar@gmail.com>
2019-06-07 18:25:53 -0400

    contact

--- Bill Bai Erfan Yeganehfar SAFEGAMES/core/src/com/safety4kids/game/Utils/GameContactListener.java
+++ Bill Bai Erfan Yeganehfar SAFEGAMES/core/src/com/safety4kids/game/Utils/GameContactListener.java
@@ -2,13 +2,11 @@ package com.safety4kids.game.Utils;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.*;
-import com.safety4kids.game.Entities.BreakableTile;
-import com.safety4kids.game.Entities.Hazard;
-import com.safety4kids.game.Entities.InteractiveTile;
-import com.safety4kids.game.Entities.MainPlayer;
+import com.safety4kids.game.Entities.*;
 
 public class GameContactListener implements ContactListener {
     private MainPlayer player;
+
     public GameContactListener(MainPlayer player) {
         this.player = player;
     }
@@ -43,8 +41,8 @@ public class GameContactListener implements ContactListener {
 //        }
 
         //If one of the fixtures is the players hat
-        if(fixA.getUserData() != null && fixB.getUserData() != null) {
-            if ((fixA.getUserData().equals("hat") || fixA.getUserData().equals("hat") && (fixA.getUserData().equals("breakable") || fixA.getUserData().equals("breakable")))) {
+        if (fixA.getUserData() != null && fixB.getUserData() != null) {
+            if ((fixA.getUserData().equals("hat") || fixB.getUserData().equals("hat") && (fixA.getUserData().equals("breakable") || fixB.getUserData().equals("breakable")))) {
 
                 if (fixA.getFilterData().categoryBits == B2DConstants.BIT_PLAYER_HAT) {
                     Gdx.app.log("obj", "collide");
@@ -54,6 +52,18 @@ public class GameContactListener implements ContactListener {
                     InteractiveTile.onHatContact(player);
             }
         }
+        if (fixA.getUserData() != null && fixB.getUserData() != null) {
+            if ((fixA.getUserData() instanceof Hazard || fixA.getUserData()instanceof Hazard)) {
+                Gdx.app.log("obj", "collide");
+                if (fixA.getFilterData().categoryBits == B2DConstants.BIT_HAZARD) {
+                    Gdx.app.log("obj", "collide");
+                    ((Hazard) fixA.getUserData()).reverseVelocity(true, false);
+                }
+            } //else
+                //((Hazard) fixB.getUserData()).reverseVelocity(true, false);
+        }
+
+
 
     }
 
