ef1707201f10fdfd8fb66c14a3f16cf1384339ac
Bill Bai <billbai0102@gmail.com>
2019-06-09 21:19:03 -0400

    Update HazardSprite.java

--- Bill Bai Erfan Yeganehfar SAFEGAMES/core/src/com/safety4kids/game/Entities/HazardSprite.java
+++ Bill Bai Erfan Yeganehfar SAFEGAMES/core/src/com/safety4kids/game/Entities/HazardSprite.java
@@ -28,9 +28,9 @@ public class HazardSprite extends Hazard {
         this.y = y;
         this.screen = screen;
         System.out.println("b" + type);
-        switch (type){
+        switch (type) {
             case 1:
-                default:
+            default:
                 frames = new Array<TextureRegion>();
                 for (int i = 0; i < 6; i++)
                     frames.add(new TextureRegion(screen.getAtlas().findRegion("Safety4Kids Stranger Sprite"), 10 + (i * 32), 4, 14, 24));
@@ -40,29 +40,30 @@ public class HazardSprite extends Hazard {
             case 2:
                 frames = new Array<TextureRegion>();
                 for (int i = 0; i < 3; i++)
-                    frames.add(new TextureRegion(screen.getAtlas().findRegion("SAFEGAMES Fire Animation"),  1+ (i * 32), 5, 18, 18));
+                    frames.add(new TextureRegion(screen.getAtlas().findRegion("SAFEGAMES Fire Animation"), 1 + (i * 32), 5, 18, 18));
                 move = new Animation(0.07f, frames);
-                setBounds(x / Safety4Kids.PPM, y / Safety4Kids.PPM, 20 / Safety4Kids.PPM, 22/ Safety4Kids.PPM);
+                setBounds(x / Safety4Kids.PPM, y / Safety4Kids.PPM, 20 / Safety4Kids.PPM, 22 / Safety4Kids.PPM);
                 break;
             case 3:
                 frames = new Array<TextureRegion>();
                 for (int i = 0; i < 7; i++)
-                    frames.add(new TextureRegion(screen.getAtlas().findRegion("PoisonSprite"),  9 + (i * 32), 3, 9, 24));
+                    frames.add(new TextureRegion(screen.getAtlas().findRegion("PoisonSprite"), 9 + (i * 32), 3, 9, 24));
                 move = new Animation(0.07f, frames);
-                setBounds(x - 20/ Safety4Kids.PPM, y / Safety4Kids.PPM, 9 / Safety4Kids.PPM, 24 / Safety4Kids.PPM);
+                setBounds(x - 20 / Safety4Kids.PPM, y / Safety4Kids.PPM, 9 / Safety4Kids.PPM, 24 / Safety4Kids.PPM);
                 break;
             case 4:
                 frames = new Array<TextureRegion>();
                 for (int i = 0; i < 7; i++)
-                    frames.add(new TextureRegion(screen.getAtlas().findRegion("SAFEGAMES Lightning Sprite"),  5 + (i * 32), 17, 19, 15));
+                    frames.add(new TextureRegion(screen.getAtlas().findRegion("SAFEGAMES Lightning Sprite"), 5 + (i * 32), 17, 19, 15));
                 move = new Animation(0.07f, frames);
-                setBounds(x - 20/ Safety4Kids.PPM, y / Safety4Kids.PPM, 19 / Safety4Kids.PPM, 15 / Safety4Kids.PPM);
+                setBounds(x - 20 / Safety4Kids.PPM, y / Safety4Kids.PPM, 19 / Safety4Kids.PPM, 15 / Safety4Kids.PPM);
                 break;
             case 5:
                 frames = new Array<TextureRegion>();
-                for(int i = 0; i < 15; i++)
-                    frames.add(new TextureRegion(screen.getAtlas().findRegion("KnifeSprite"), 11 + (i*32), 8, 14 ,17));
-                  setBounds(x - 20/ Safety4Kids.PPM, y / Safety4Kids.PPM, 14 / Safety4Kids.PPM, 17 / Safety4Kids.PPM);
+                for (int i = 0; i < 15; i++)
+                    frames.add(new TextureRegion(screen.getAtlas().findRegion("KnifeSprite"), 11 + (i * 32), 8, 14, 17));
+                move = new Animation(0.07f, frames);
+                setBounds(x - 20 / Safety4Kids.PPM, y / Safety4Kids.PPM, 14 / Safety4Kids.PPM, 17 / Safety4Kids.PPM);
                 break;
             case 6:
                 break;
@@ -72,18 +73,19 @@ public class HazardSprite extends Hazard {
 
     }
 
-    public void update(float dt){
+    public void update(float dt) {
         time += dt;
-        setPosition(b2body.getPosition().x - getWidth() / 2,b2body.getPosition().y - getHeight() / 2 );
+        setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
         setRegion(move.getKeyFrame(time, true));
 
     }
+
     @Override
     protected void createHazard() {
         //Defined Body
         BodyDef bdef = new BodyDef();
         //Position of the body
-        bdef.position.set(getX() / Safety4Kids.PPM, getY()/Safety4Kids.PPM);
+        bdef.position.set(getX() / Safety4Kids.PPM, getY() / Safety4Kids.PPM);
 
         //the type of Body is dynamic (therefore it can move)
         bdef.type = BodyDef.BodyType.DynamicBody;
@@ -96,9 +98,9 @@ public class HazardSprite extends Hazard {
         //The type of shape is assigned and defined
         PolygonShape shape = new PolygonShape();
 
-        switch(type) {
+        switch (type) {
             case 1:
-                default:
+            default:
                 shape.setAsBox(5f / Safety4Kids.PPM, 13f / Safety4Kids.PPM);
                 break;
             case 2:
@@ -112,7 +114,7 @@ public class HazardSprite extends Hazard {
         fdef.filter.categoryBits = B2DConstants.BIT_HAZARD;
         //Defines what the player can
         fdef.filter.maskBits = B2DConstants.PLATFORM_BIT | B2DConstants.BIT_COIN |
-               B2DConstants.BIT_BREAKABLE_BLOCK | B2DConstants.BIT_HAZARD |
+                B2DConstants.BIT_BREAKABLE_BLOCK | B2DConstants.BIT_HAZARD |
                 B2DConstants.BIT_OBJECT | B2DConstants.BIT_PLAYER;
 
         //the shape is bound to the fixture, and the fixture to the body
@@ -122,7 +124,7 @@ public class HazardSprite extends Hazard {
 
     }
 
-    public void draw(Batch batch){
+    public void draw(Batch batch) {
         super.draw(batch);
     }
 }
