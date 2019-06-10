f8b6e930a3201779525aed38bd6947346b557ca6
TOBAIS123 <erfanyeganehfar@gmail.com>
2019-06-10 00:58:53 -0400

    commenting

--- Bill Bai Erfan Yeganehfar SAFEGAMES/core/src/com/safety4kids/game/Entities/HazardSprite.java
+++ Bill Bai Erfan Yeganehfar SAFEGAMES/core/src/com/safety4kids/game/Entities/HazardSprite.java
@@ -10,24 +10,50 @@ import com.badlogic.gdx.utils.Array;
 import com.safety4kids.game.Safety4Kids;
 import com.safety4kids.game.Screens.GameScreen;
 import com.safety4kids.game.Utils.B2DConstants;
-
+/**
+ * This Class represents the main hazard type, it defines the textures, box2d body, and updates the position and texture regions of the hazard
+ *
+ * <h2>Course Info:</h2>
+ *
+ * @version 4.2 2019-06-02
+ * @author Erfan Yeganehfar
+ * @author Bill Bai
+ *
+ * Ms. Krasteva
+ *
+ * Modifications:
+ * 4.0 Finised the constructor added instance variables, and defined the hazrad in box2d
+ * 4.1 Added the texture region of the hazard
+ * 4.2 Added the different types of hazards and their texture regions
+ */
 public class HazardSprite extends Hazard {
 
+    /** The current amount of time in which the hazards animation is taking**/
     private float time;
+
+    /** The texture region array used to read the textures from the .pack file **/
     private Array<TextureRegion> frames;
+
+    /**The actual animation set of texture regions used to animate the sprite **/
     private Animation<TextureRegion> move;
-    private float x;
-    private float y;
-    private GameScreen screen;
+
+    /**The type of hazard that this class represents**/
     private int type;
 
-
+    /**
+     * The constructor for the HazardSprite class, it instantiates the screen and position of the sprite, as well as defines the box2d body,
+     * and the texture animation for it
+     *
+     * @param screen the screen in which the hazard is drawn on
+     * @param x the x location of the hazard
+     * @param y the y loaction of the hazard
+     * @param type the type of hazard
+     */
     public HazardSprite(GameScreen screen, float x, float y, int type) {
         super(screen, x, y);
-        this.x = x;
-        this.y = y;
-        this.screen = screen;
+        this.type = type;
 
+        //Switch used to draw the different types of hazards
         switch (type) {
             case 1:
             default:
@@ -47,7 +73,7 @@ public class HazardSprite extends Hazard {
             case 3:
                 frames = new Array<TextureRegion>();
                 for (int i = 0; i < 7; i++)
-                    frames.add(new TextureRegion(screen.getAtlas().findRegion("PoisonSprite"), 9 + (i * 32), 3, 9, 24));
+                    frames.add(new TextureRegion(screen.getAtlas().findRegion("PoisonSprite"), 9 + (i * 32), 9, 9, 24));
                 move = new Animation(0.07f, frames);
                 setBounds(x / Safety4Kids.PPM, y / Safety4Kids.PPM, 9 / Safety4Kids.PPM, 24 / Safety4Kids.PPM);
                 break;
@@ -92,6 +118,10 @@ public class HazardSprite extends Hazard {
 
     }
 
+    /**
+     * Update used to update the position of the hazard as well as the current animation
+     * @param dt uses delta time derived from the the cpu's speed indicating the frames of the game
+     */
     public void update(float dt) {
         time += dt;
         setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
@@ -99,6 +129,9 @@ public class HazardSprite extends Hazard {
 
     }
 
+    /**
+     * Creates the hazards box2d body inside the screen box2d world
+     */
     @Override
     protected void createHazard() {
         //Defined Body
@@ -114,13 +147,14 @@ public class HazardSprite extends Hazard {
 
         //Now a fixture is made for collisions
         FixtureDef fdef = new FixtureDef();
+
         //The type of shape is assigned and defined
         PolygonShape shape = new PolygonShape();
         shape.setAsBox(11f / Safety4Kids.PPM, 10f / Safety4Kids.PPM);
 
-        //Sets the filtering bits of the body as the Player bit category
+        //Sets the filtering bits of the body as the hazards bit category
         fdef.filter.categoryBits = B2DConstants.BIT_HAZARD;
-        //Defines what the player can
+        //Defines what the hazards can collide with
         fdef.filter.maskBits = B2DConstants.PLATFORM_BIT | B2DConstants.BIT_COIN |
                 B2DConstants.BIT_BREAKABLE_BLOCK | B2DConstants.BIT_HAZARD |
                 B2DConstants.BIT_OBJECT | B2DConstants.BIT_PLAYER;
@@ -132,6 +166,10 @@ public class HazardSprite extends Hazard {
 
     }
 
+    /**
+     * The batch to be drawn to the screen
+     * @param batch the batch to be drawn
+     */
     public void draw(Batch batch) {
         super.draw(batch);
     }
